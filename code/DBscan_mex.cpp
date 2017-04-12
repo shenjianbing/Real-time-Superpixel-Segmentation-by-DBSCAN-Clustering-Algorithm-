90712db981c955ccd4ef349463c5f182f05b4a7e
Peter Huang <peterhuang2015@outlook.com>
2017-04-12 14:04:13 +0800

    Update label array and reformatting code
    In Chinese:
    实验室内部使用 DBScan 时程序会 Crash，查到原因在于最后 label 数字随机排但大多数应用都是从 1 到 n 分配 label 值，在尾部代码作了替换并更新到这里。
    In English:
    Label number should be assigned from 1 to n according to convention, otherwise make program utilizing this algorithm may crash.

--- code/DBscan_mex.cpp
+++ code/DBscan_mex.cpp
@@ -1,61 +1,80 @@
-#include<mex.h>
-#include<matrix.h>
-#include<time.h>
-#include"DBscan.h"
+#include <mex.h>
+#include <matrix.h>
+#include <map>
+#include <time.h>
+#include "DBscan.h"
 
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
-{  
-	unsigned char* R,* G,* B;
+{
+	unsigned char* R, *G, *B;
 	int Rows;int Cols;
 	unsigned short* label;
 	unsigned short* output;
-    double supnumber;
-    int post;
+	double supnumber;
 	unsigned char* img;
-    int *outputNumSuperpixels;
-	
+	int *outputNumSuperpixels;
 
-	Rows=mxGetM(prhs[0]);
-	Cols=mxGetN(prhs[0])/3;
-	img=(unsigned char*)mxGetPr(prhs[0]);
-    supnumber=((double*)(mxGetPr(prhs[1])))[0];
-    post=((double*)(mxGetPr(prhs[2])))[0];
 
-	int pixel=Rows*Cols;
-	R=new  unsigned char[pixel];
-	G=new  unsigned char[pixel];
-	B=new  unsigned char[pixel];
-	label=new unsigned short[pixel];
-	
-     for(int j=0;j<Cols;j++)
-       for(int i=0;i<Rows;i++)
-	{
-		R[i*Cols+j]=img[j*Rows+i];
-		G[i*Cols+j]=img[j*Rows+i+pixel];
-		B[i*Cols+j]=img[j*Rows+i+pixel+pixel];
-        label[j*Rows+i]=0;
+	Rows = mxGetM(prhs[0]);
+	Cols = mxGetN(prhs[0]) / 3;
+	img = (unsigned char*)mxGetPr(prhs[0]);
+	supnumber = ((double*)(mxGetPr(prhs[1])))[0];
+
+	int pixel = Rows*Cols;
+	R = new  unsigned char[pixel];
+	G = new  unsigned char[pixel];
+	B = new  unsigned char[pixel];
+	label = new unsigned short[pixel];
+
+	for (int j = 0;j < Cols;j++)
+		for (int i = 0;i < Rows;i++)
+		{
+			R[i*Cols + j] = img[j*Rows + i];
+			G[i*Cols + j] = img[j*Rows + i + pixel];
+			B[i*Cols + j] = img[j*Rows + i + pixel + pixel];
+			label[j*Rows + i] = 0;
+		}
+
+	//--------------------------
+	//  Compute superpixels
+	//--------------------------
+	int realnumber = 0;
+	DBscan(R, G, B, Rows, Cols, label, supnumber, realnumber);
+
+
+	plhs[0] = mxCreateNumericMatrix(Rows, Cols, mxUINT16_CLASS, mxREAL);
+	output = (unsigned short*)mxGetPr(plhs[0]);
+
+	//Sort labels from 1 to max, use heap instead of vector can make it faster
+	map<unsigned short, unsigned short> o2n; // old label to new label, unorganized to organized number
+	//vector<unsigned short> old_label;
+	int pixel_num = pixel, new_label = 1;
+	map<unsigned short, unsigned short>::iterator mit;
+	//vector<unsigned short>::iterator vit;
+	for (int i = 0; i < pixel_num; i++) {
+		int old_label = label[i];
+		if (o2n.find(old_label) == o2n.end()) {
+			// Assign new label number
+			o2n[old_label] = new_label;
+			new_label++;
+		}
 	}
 
-    //--------------------------
-    //  Compute superpixels
-    //--------------------------
-    int realnumber=0;
-    DBscan(R,G,B,Rows,Cols,label,supnumber,realnumber,post);
-    
-    
-	plhs[0]=mxCreateNumericMatrix(Rows,Cols,mxUINT16_CLASS,mxREAL);
-	output=(unsigned short*)mxGetPr(plhs[0]);
+	for (int i = 0; i < pixel_num; i++) {
+		label[i] = o2n[label[i]];
+	}
 
-   for(int j=0;j<Cols;j++)
-    {
-        for(int i=0;i<Rows;i++)
-		output[j*Rows+i]=label[i*Cols+j];
-    }
-    
-    delete [] R;
-	delete [] G;
- 	delete [] B;
-	delete [] label;
+
+	for (int j = 0;j < Cols;j++)
+	{
+		for (int i = 0;i < Rows;i++)
+			output[j*Rows + i] = label[i*Cols + j];
+	}
+
+	delete[] R;
+	delete[] G;
+	delete[] B;
+	delete[] label;
 
 }
